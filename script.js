32a9d6e147ca0fd576d9c98fac73ebe5dcda94d6
Hamza Anwar <hamzaanwardev@gmail.com>
2021-08-14 01:44:52 +0500

    JavaScript Compelted

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const load_text = document.querySelector(".loading-text");
 const bg = document.querySelector(".bg");
-
+// time interval
 let int = setInterval(blurring, 30);
 
 let load = 0;
@@ -14,7 +14,9 @@ function blurring() {
     load_text.style.opacity = scale(load, 0, 100, 1, 0);
     bg.style.filter = `blur(${scale (load, 0, 100, 30, 0)}px)`;
 }
-
+// Using scale for blur effect and opacity from stack overflow (link below)
 const scale = (number, inMin, inMax, outMin, outMax) => {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
