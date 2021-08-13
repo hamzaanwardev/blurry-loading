7c1c35089f15671aa8d5330f5e44d2a7f34e1fa6
Hamza Anwar <hamzaanwardev@gmail.com>
2021-08-14 01:05:01 +0500

    Html done

---
+++ script.js
@@ -0,0 +1,19 @@
+const load_text = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let int = setInterval(blurring, 30);
+
+   let load = 0;
+function blurring () {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    } 
+    load_text.innerText = `${load}%`;
+    load_text.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur (${scale (load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (number, inMin, inMax, outMin, outMax) ;
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
