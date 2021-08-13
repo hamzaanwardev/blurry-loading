8c58be131f63d333b1337ec83c4977fdcaed1844
Hamza Anwar <hamzaanwardev@gmail.com>
2021-08-14 01:37:28 +0500

    Update JavaScript file

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ const bg = document.querySelector(".bg");
 
 let int = setInterval(blurring, 30);
 
-   let load = 0;
-function blurring () {
-    load++
+let load = 0;
+function blurring() {
+    load++;
 
-    if (load > 99) {
+    if(load > 99){
         clearInterval(int)
     } 
     load_text.innerText = `${load}%`;
     load_text.style.opacity = scale(load, 0, 100, 1, 0);
-    bg.style.filter = `blur (${scale (load, 0, 100, 30, 0)}px)`;
+    bg.style.filter = `blur(${scale (load, 0, 100, 30, 0)}px)`;
 }
 
-const scale = (number, inMin, inMax, outMin, outMax) ;
+const scale = (number, inMin, inMax, outMin, outMax) => {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
